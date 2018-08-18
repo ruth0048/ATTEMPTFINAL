4b34f255d925b0f6dae43a8912fdef5365c721bf
fill0027 <31836986+fill0027@users.noreply.github.com>
2018-08-18 15:32:40 -0400

    here be my commit! lots of stuff!

--- BOATS'/Assets/UpPressure.cs
+++ BOATS'/Assets/UpPressure.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class UpPressure : MonoBehaviour {
 
     private Rigidbody myRB;
-    public float maxHeight=1.25f;
+    private float maxHeight;//=1.25f
+    public float pressure=10.0f;
+
+    //lowest point variable?
+    //random up and down?
+    //match waves
+
 	// Use this for initialization
 	void Start () {
         myRB = GetComponent<Rigidbody>();
+        maxHeight = myRB.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (myRB.position.y < maxHeight)
         {
-            Vector3 myVector = new Vector3(0.0f, 10.0f, 0.0f);
+            Vector3 myVector = new Vector3(0.0f, pressure, 0.0f);
             myRB.AddForce(myVector, ForceMode.Force);
         }
         else
---
+++ BOATS'/Assets/WinChecker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinChecker : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="boat")
+        {
+            Debug.LogWarning("WIN");
+        }
+    }
+}
