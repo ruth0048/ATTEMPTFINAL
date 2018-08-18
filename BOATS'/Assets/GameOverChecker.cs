4012b4b084ebc31ff9f7481e4fd384864ad8640a
fill0027 <31836986+fill0027@users.noreply.github.com>
2018-08-18 16:49:42 -0400

    walls and win/loss states!

---
+++ BOATS'/Assets/GameOverChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverChecker : MonoBehaviour {
+
+    //timer for touching long enough
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="boat")
+        {
+            Debug.LogWarning("GameOver");
+        }
+    }
+}
--- BOATS'/Assets/WinChecker.cs
+++ BOATS'/Assets/WinChecker.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WinChecker : MonoBehaviour {
+    public GameObject blackOut;
+    //private bool win = false;
+    private void Awake()
+    {
+        //FindObjectOfType<Camera>().Get
+    }
 
+    //private void Update()
+    //{
+    //    if(win)
+    //    {
+    //        fade();
+    //    }
+    //}
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag=="boat")
         {
+            //win = true;
             Debug.LogWarning("WIN");
         }
     }
